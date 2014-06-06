d4e6e60dcbd3525ee0278bb70a22844f16684a16
Chaim-Leib Halbert <chaimleib.halbert@scorm.com>
2014-06-06 09:55:40 -0500

    added template_test.py

---
+++ template_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+def raise_exception(*args, **kwargs):
+    print "Called raise_exception({args}, {kwargs})" % (args, kwargs)
+    raise Exception()
+
+
+# TODO
+class TemplateTest(unittest.TestCase):
+
+    def setUp(self):
+        # Set up variables before each test
+        pass
+
+    def tearDown(self):
+        # Tear down variables after each test
+        pass
+
+    def test_test(self):
+        self.assertTrue(True, "True should be true!")
+        self.assertFalse(False, "False should be false!")
+        self.assertEquals(1, 1, "1 should equal 1!")
+        self.assertNotEqual(1, 2, "1 should not equal 2!")
+        self.assertRaises(Exception, raise_exception, 1, 2, c=3, d=4)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TemplateTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
