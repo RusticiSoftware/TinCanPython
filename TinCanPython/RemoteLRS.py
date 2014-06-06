99066c8666cef264f88fa0061738cf68a17f7ea3
Jake Noble <jakenoble@Jakes-MacBook-Pro.local>
2014-06-06 10:35:02 -0500

    Updated send_request and added a few more stubbed functions

--- TinCanPython/RemoteLRS.py
+++ TinCanPython/RemoteLRS.py
@@ -26,7 +26,7 @@ class RemoteLRS(object):
             url = request.resource
         else:
             url = self.endpoint
-            if url.endswith("/"):
+            if not url.endswith("/"):
                 url += "/"
             url += request.resource
 
@@ -45,9 +45,9 @@ class RemoteLRS(object):
         params = urllib.urlencode(params)
 
         if parsed.scheme == "https":
-            web_req = httplib.HTTPSConnection(parsed.hostname)
+            web_req = httplib.HTTPSConnection(parsed.hostname, parsed.port)
         else:
-            web_req = httplib.HTTPConnection(parsed.hostname)
+            web_req = httplib.HTTPConnection(parsed.hostname, parsed.port)
 
         path = parsed.path
         if params:
@@ -62,9 +62,6 @@ class RemoteLRS(object):
 
         if (200 <= response.status < 300) or (response.status == 404 and request.ignore404):
             success = True
-        elif 300 <= response.status < 400:
-            raise Exception("Bad Response Status Code: " + response.status)
-            success = False
         else:
             success = False
 
@@ -75,9 +72,8 @@ class RemoteLRS(object):
         lrs_response = self.send_request(request)
 
         if lrs_response.success:
+            #TODO: set lrs response from response content from JSON
             pass
-            #TODO:
-            #lrs_response.content = lrs_response.response.content (from JSON)
 
         return lrs_response
 
@@ -97,8 +93,106 @@ class RemoteLRS(object):
 
         if lrs_response.success:
             if statement.id is not None:
+                #TODO: set statement id from lrs response
                 pass
-                #lrs_response.id = #JSON decoded statement id
             lrs_response.content = statement
 
         return lrs_response
+
+    def save_statements(self, statements):
+        #TODO: verify all statements are instances of statements and versioned to this.version
+
+        request = HTTPRequest.HTTPRequest(endpoint=self.endpoint, resource="statements", method="POST")
+        request.headers["Content-Type"] = "application/json"
+        #request.content = #JSON encoded statements
+
+        lrs_response = self.send_request(request)
+
+        if lrs_response.success:
+            #TODO: set id for each statement form lrs response
+            lrs_response.content = statements
+
+        return lrs_response
+
+    def retrieve_statement(self, id):
+        request = HTTPRequest.HTTPRequest(endpoint=self.endpoint, method="GET", resource="statements")
+        request.query_params["statementId"] = id
+
+        lrs_response = self.send_request(request)
+
+        if lrs_response.success:
+            #TODO: set response content = statement from response
+            pass
+
+        return lrs_response
+
+    def retrieve_voided_statement(self, id):
+        request = HTTPRequest.HTTPRequest(endpoint=self.endpoint, method="GET", resource="statements")
+        request.query_params["voidedStatementId"] = id
+
+        lrs_response = self.send_request(request)
+
+        if lrs_response.success:
+            #TODO: set response content = statement from response
+            pass
+
+        return lrs_response
+
+    def query_statements(self, query):
+        params = {}
+
+        other_params = ["registration", "since", "until", "limit", "ascending", "related_activities",
+                        "related_agents", "format", "attachments"]
+
+        for k,v in query.items():
+            if v is not None:
+                if k == "agent":
+                    pass
+                    #TODO: params[k] = #JSON version encoded v (agent)
+                elif k == "verb" or k == "activity":
+                    pass
+                    #TODO: params[k] = v.id
+                elif k in other_params:
+                    params[k] = v
+
+            request = HTTPRequest.HTTPRequest(endpoint=self.endpoint, method="GET", resource="statements")
+            request.query_params = params
+
+            lrs_response = self.send_request(request)
+
+            if lrs_response.success:
+                pass
+                #TODO: lrs_response.content = #StatementsResult from response content from JSON
+
+            return lrs_response
+
+    def more_statements(self, more_url):
+        #TODO: if more_url is a StatementsResult obj, more_url = more_url.more_url
+        more_url = self.get_endpoint_server_root() + more_url
+
+        request = HTTPRequest.HTTPRequest(endpoint=self.endpoint, method="GET", resource=more_url)
+
+        lrs_response = self.send_request(request)
+
+        if lrs_response.success:
+            pass
+            #TODO: lrs_response.content = #StatementsResult from response content from JSON
+
+        return lrs_response
+
+    def get_endpoint_server_root(self):
+        parsed = urlparse(self.endpoint)
+        root = parsed.scheme + "://" + parsed.hostname
+
+        if parsed.port is not None:
+            root += ":" + str(parsed.port)
+
+        return root
+
+
+
+
+
+
+
+
