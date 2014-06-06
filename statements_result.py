219bd59866f07c7b91210adbf2b994fcd4e0ee89
Chaim-Leib Halbert <chaimleib.halbert@scorm.com>
2014-06-06 13:52:13 -0500

    added documentation

--- statements_result.py
+++ statements_result.py
@@ -5,7 +5,14 @@ import json
 
 class StatementsResult(object):
     """
-    Statements result model class, returned by LRS calls to get multiple statements
+    Statements result model class, returned by LRS calls to get
+    multiple statements.
+
+    Attributes:
+     * ``statements`` - a list containing partial results for an LRS
+    query.
+     * ``more`` - If there are more results, a URL pointing to the
+     rest. None otherwise.
     """
     def __init__(self, json_str):
         self.statements = []
