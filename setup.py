94b3b0c68eed21dbe3e19fa5da4213ac7209043b
Jake Noble <jakenoble@Jakes-MacBook-Pro.local>
2014-06-18 14:40:38 -0500

    Setup files for PyPI.  Reformatted remote lrs to match rest of library.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='tincan',
+    packages=[
+        'tincan'
+    ],
+    version='1.0',
+    description='A Python library for implementing Tin Can API.',
+    author='Rustici Software',
+    author_email='mailto:info@tincanapi.com',
+    url='https://github.com/RusticiSoftware/TinCanPython',
+    license='Apache License 2.0',
+
+    #TODO:this needs to be setup
+    download_url='https://github.com/RusticiSoftware/TinCanPython/tarball/1.0',
+    keywords=[
+        'Tin Can',
+        'Tin Can API',
+        'Experience API'
+    ],
+    requires=[
+      'aniso8601'
+    ]
+)
