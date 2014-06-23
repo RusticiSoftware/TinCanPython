ce31b13bb73f79bdbea64c15f35be87a73ec72b9
Jake Noble <jakenoble@Jakes-MacBook-Pro.local>
2014-06-23 10:44:50 -0500

    fixed document imports

--- tincan/documents/state_document.py
+++ tincan/documents/state_document.py
@@ -11,7 +11,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-from document import Document
+from tincan.documents import Document
 from tincan.agent import Agent
 from tincan.activity import Activity
 import uuid
