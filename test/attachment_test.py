b0f990fe4551c81b7c869df6960ad9abc7bf9d76
Chaim-Leib Halbert <chaimleib.halbert@scorm.com>
2014-06-24 13:44:14 -0500

    removed hashbang, whitespace fix

--- test/attachment_test.py
+++ test/attachment_test.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Rustici Software
+#    Copyright 2014 Rustici Software
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
