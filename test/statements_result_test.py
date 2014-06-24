918a0bf68f9efe5a56edf35de09a6b392972e9bb
Nolan <nolan@Nolans-MacBook-Pro.local>
2014-06-24 12:38:54 -0500

    Fixed StatementsResult tests

--- test/statements_result_test.py
+++ test/statements_result_test.py
@@ -15,25 +15,28 @@
 #    limitations under the License.
 
 import unittest
+import uuid
 
 if __name__ == '__main__':
     from main import setup_tincan_path
     setup_tincan_path()
 from tincan.statements_result import StatementsResult
+from tincan.statement import Statement
 from test_utils import TinCanBaseTestCase
-
 
 class StatementsResultTest(TinCanBaseTestCase):
     def test_serialize_deserialize(self):
         sr = StatementsResult()
-        sr.statements = [1, 2, 3, ]
+        uuid_str = '016699c6-d600-48a7-96ab-86187498f16f'
+        sr.statements = [Statement(id=uuid_str), Statement(id=uuid_str), Statement(id=uuid_str), ]
         sr.more = 'http://www.example.com/more/1234'
 
         self.assertSerializeDeserialize(sr)
 
     def test_serialize_deserialize_init(self):
+        uuid_str = '016699c6-d600-48a7-96ab-86187498f16f'
         data = {
-            'statements': [1, 2, 3, 4, ],
+            'statements': [Statement(id=uuid_str), Statement(id=uuid_str), Statement(id=uuid_str), Statement(id=uuid_str), ],
             'more': 'http://www.example.com/more/1234',
         }
 
@@ -44,11 +47,12 @@ class StatementsResultTest(TinCanBaseTestCase):
         sr = StatementsResult()
         self.assertEqual(len(sr.statements), 0, 'Empty StatementsResult inited as non-empty!')
 
-        sr.statements = (1, 2, 3,)
+        uuid_str = '016699c6-d600-48a7-96ab-86187498f16f'
+        sr.statements = (Statement(id=uuid_str), Statement(id=uuid_str), Statement(id=uuid_str),)
         self.assertIsInstance(sr.statements, list, 'Did not convert tuple to list!')
 
-        sr.statements.append(4)
-        self.assertEqual(sr.statements[3], 4, 'Did not append value!')
+        sr.statements.append(Statement(id=uuid_str))
+        self.assertEqual(sr.statements[3].id, uuid.UUID('016699c6-d600-48a7-96ab-86187498f16f'), 'Did not append value!')
 
         self.assertIsNone(sr.more)
 
