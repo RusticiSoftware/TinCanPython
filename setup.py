cf23461d308046ee0549497159b708a9b03d2424
Eric Herrera <eric.herrera@edunext.co>
2020-05-17 21:15:19 -0500

    Add setup.py install_requirements definition. Update travis file using new requirements. Create requirements folder. create pip_tools, base, test and travis requirements files. Add Makefile. Add makefile upgrade command. Include requirements files generated using upgrade command. Add openedx.yaml and include OEPs list. Add python 3.8 to tests.

--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
 from setuptools import setup
 
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
 setup(
     name='edx-tincan-py35',
     packages=[
@@ -11,6 +35,11 @@ setup(
     description='A Python 3 library for implementing Tin Can API.',
     author='edX',
     author_email='oscm@edx.org',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+    ],
     maintainer_email='mailto:brian.miller@tincanapi.com',
     url='http://rusticisoftware.github.io/TinCanPython/',
     license='Apache License 2.0',
@@ -24,8 +53,5 @@ setup(
         'SCORM',
         'AICC',
     ],
-    install_requires=[
-        'aniso8601',
-        'pytz',
-    ],
+    install_requires=load_requirements('requirements/base.in'),
 )
